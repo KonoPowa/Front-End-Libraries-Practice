82041e914de2c2c6903628ec44abc59d2bbb6d12
Yacob Abusadah <50770569+KonoPowa@users.noreply.github.com>
2020-05-24 11:40:28 -0500

    Update react.jsx

--- react.jsx
+++ react.jsx
@@ -542,3 +542,38 @@ class MyForm extends React.Component {
     );
   }
 };
+
+
+
+
+
+
+//React: Pass State as Props to Child Components******************
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         <Navbar name = {this.state.name} />
+       </div>
+    );
+  }
+};
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      <h1>Hello, my name is: {this.props.name} </h1>
+    </div>
+    );
+  }
+};
